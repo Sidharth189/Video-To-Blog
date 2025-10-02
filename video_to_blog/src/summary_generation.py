4450ee6bb7f15b503563c49c8c4985747877d7c9
warrior189 <sidhathsshaji189@gmail.com>
2025-10-03 03:07:03 +0530

    ''

---
+++ video_to_blog/src/summary_generation.py
@@ -0,0 +1,10 @@
+from llm import llm_call
+
+def summary_generation(transcript):
+    prompt=f'''Read the below given transcript carefully and generate appropriate title for the it with label TITLE 
+            and generate a brief summary that points out important points from the transcript with label SUMMARY
+
+          transcript: {transcript}
+    '''
+    summary=llm_call(transcript)
+    return summary
